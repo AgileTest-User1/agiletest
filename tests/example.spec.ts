a3ba73c34d654e37c82bef41bdc89695e45ab519
Tran Ton Dai Nghia <peter@devsamurai.com>
2025-02-24 14:16:15 +0700

    update username

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,7 +7,19 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("get started link", async ({ page }) => {
+test("get started section", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
+
+test("empty username", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Click the get started link.
